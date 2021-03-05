28190ca03886cf6f023fed2a3bf36506f6c87e3f
Huacai Sang <sanghuacai@gmail.com>
2021-03-05 23:08:16 +0100

    add cast number

--- ast/cast.go
+++ ast/cast.go
@@ -58,81 +58,48 @@ func CastFromPointer(b *ir.Block, value ir.Value, typ ir.Type) ir.Value {
 	return nil
 }
 
-func CastNumberExpr(in ir.Constant, out ir.Type) ir.Constant {
-	return in
-	//TO-DO implement
+func CastNumberExpr(from ir.Constant, to ir.Type) ir.Constant {
+	t := from.Type()
+	if ir.IsInt(t) {
+		i := t.(*ir.IntType)
+		if ir.IsInt(to) {
+			if i.Unsigned {
+				return ir.NewExprZExt(from, to)
+			}
+			return ir.NewExprSExt(from, to)
+		}
+		if i.Unsigned {
+			return ir.NewExprUIToFP(from, to)
+		}
+		return ir.NewExprSIToFP(from, to)
+	}
+	return ir.NewExprFPExt(from, to)
 }
 
-func CastNumber(c *Context, in ir.Value, out ir.Type) ir.Value {
-	return in
-	//TO-DO implement
-	/*
-		inSize := NumberSize(in.Type())
-		outSize := NumberSize(out)
-
-		if ir.Equal(in.Type(), out) {
-			return in
-		}
-
-		if i, ok := in.(*ir.Int); ok && ir.IsInt(out) {
-			i.Typ = out.(*ir.IntType)
-			return i
-		}
-
-		if f, ok := in.(*ir.Float); ok && ir.IsFloat(out) {
-			f.Typ = out.(*ir.FloatType)
-			return f
-		}
-
-		if ir.Equal(to, ir.Void) {
-			return nil, nil
-		}
-
-		if ir.IsPointer(inType) && ir.IsPointer(to) {
-			return prog.Compiler.CurrentBlock().NewBitCast(in, to), nil
-		}
-
-		if fromFloat && toInt {
-			return prog.Compiler.CurrentBlock().NewFPToSI(in, to), nil
-		}
-
-		if fromInt && toFloat {
-			return prog.Compiler.CurrentBlock().NewSIToFP(in, to), nil
-		}
-
-		if fromInt && toInt {
-			if inSize < outSize {
-				return prog.Compiler.CurrentBlock().NewSExt(in, to), nil
+func CastNumber(c *Context, from ir.Value, to ir.Type) ir.Value {
+	// will not check if valid, since PromoteNumberType is called
+	// before this which already checked error
+	var cast ir.Instruction
+	t := from.Type()
+	if ir.IsInt(t) {
+		i := t.(*ir.IntType)
+		if ir.IsInt(to) {
+			if i.Unsigned {
+				cast = ir.NewZExt(from, to)
+			} else {
+				cast = ir.NewSExt(from, to)
 			}
-			if inSize == outSize {
-				return in, nil
+		} else {
+			if i.Unsigned {
+				cast = ir.NewUIToFP(from, to)
+			} else {
+				cast = ir.NewSIToFP(from, to)
 			}
-			return prog.Compiler.CurrentBlock().NewTrunc(in, to), nil
 		}
+	} else {
+		cast = ir.NewFPExt(from, to)
+	}
 
-		if fromFloat && toFloat {
-			if inSize < outSize {
-				return prog.Compiler.CurrentBlock().NewFPExt(in, to), nil
-			}
-			if inSize == outSize {
-				return in, nil
-			}
-			return prog.Compiler.CurrentBlock().NewFPTrunc(in, to), nil
-		}
-
-		// If the cast would not change the type, just return the in value, nil
-		if ir.Equal(inType, to) {
-			return in, nil
-		}
-
-		if ir.IsPointer(inType) && ir.IsInt(to) {
-			return prog.Compiler.CurrentBlock().NewPtrToInt(in, to), nil
-		}
-
-		if ir.IsInt(inType) && ir.IsPointer(to) {
-			return prog.Compiler.CurrentBlock().NewIntToPtr(in, to), nil
-		}
-
-		return nil, fmt.Errorf("Failed to typecast type %s to %s", inType.String(), to)
-	*/
+	c.Block.AddInstruction(cast)
+	return cast.(ir.Value)
 }
